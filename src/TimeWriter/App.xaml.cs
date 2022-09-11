106a99808b63cf47fb46d600a1407d82e23a555a
chrisjohnson2134 <chrisjohnson7690@gmail.com>
2022-09-11 13:01:05 -0500

    Rounded All Buttons edges.

--- src/TimeWriter/App.xaml.cs
+++ src/TimeWriter/App.xaml.cs
@@ -1,13 +1,6 @@
 ﻿using Prism.Ioc;
 using Prism.Unity;
-using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Navigation;
 using TimeWriter.Framework.DataRepos;
 using TimeWriter.Framework.TaskItem;
 using TimeWriter.Framework.UserProfile;
